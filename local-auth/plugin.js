872a9e829708fc748b4d3f3c764b8843d6f5a970
John Beech <john.beech@bbc.co.uk>
2019-03-19 13:15:44 +0000

    Apply standard js linting and remove dead code

--- local-auth/plugin.js
+++ local-auth/plugin.js
@@ -1,103 +1,96 @@
-var crypto = require('crypto');
-var passport = require('passport');
-var passportLocal = require('passport-local');
-var bodyParser = require('body-parser');
+const path = require('path')
+const crypto = require('crypto')
+const passport = require('passport')
+const passportLocal = require('passport-local')
 
-var defaultConfig = {};
+const defaultConfig = {}
 
-function create() {
-    var pluginConfig = defaultConfig;
+function create () {
+  let pluginConfig = defaultConfig
 
-    var User = {
-        findOne: function(username) {
-            var user = pluginConfig.users[username] || false;
-            return Promise.resolve(user);
-        }
-    };
+  function getConfig () {
+    return pluginConfig
+  }
 
-    function getConfig() {
-        return pluginConfig;
-    }
+  function setConfig (config) {
+    pluginConfig = config
+  }
 
-    function setConfig(config) {
-        pluginConfig = config;
-    }
+  function info () {
+    return require('../package.json')
+  }
 
-    function info() {
-        return require('../package.json');
-    }
+  const sha1sum = function (input) {
+    return crypto.createHash('sha1').update(JSON.stringify(input)).digest('hex')
+  }
 
-    var sha1sum = function(input) {
-        return crypto.createHash('sha1').update(JSON.stringify(input)).digest('hex')
-    }
+  function registerStrategy (app) {
+    const users = app.users
+    passport.use(new passportLocal.Strategy(
+      function (username, password, done) {
+        const matcher = (user) => (user.username + '').toLowerCase() === (username + '').toLowerCase()
+        users.findUsersBy(matcher)
+          .then(function (users) {
+            const user = users[0] || false
+            if (!user) {
+              console.log('[Local Auth] Invalid username', username)
+              return done(null, false, {
+                message: 'Incorrect username.'
+              })
+            }
+            if (user.passwordHash !== sha1sum(password)) {
+              console.log('[Local Auth] Invalid password provided for', username)
+              return done(null, false, {
+                message: 'Incorrect password.'
+              })
+            }
+            console.log('[Local Auth] Authenticated user', username)
+            return done(null, user)
+          })
+          .catch(done)
+      }))
+  }
 
-    function registerStrategy(app) {
-        var users = app.users;
-        passport.use(new passportLocal.Strategy(
-            function(username, password, done) {
-                const matcher = (user) => (user.username + '').toLowerCase() === (username + '').toLowerCase();
-                users.findUsersBy(matcher)
-                    .then(function(users) {
-                        const user = users[0] || false;
-                        if (!user) {
-                            console.log('[Local Auth] Invalid username', username);
-                            return done(null, false, {
-                                message: 'Incorrect username.'
-                            });
-                        }
-                        if (user.passwordHash !== sha1sum(password)) {
-                            console.log('[Local Auth] Invalid password provided for', username);
-                            return done(null, false, {
-                                message: 'Incorrect password.'
-                            });
-                        }
-                        console.log('[Local Auth] Authenticated user', username);
-                        return done(null, user);
-                    })
-                    .catch(done);
-            }));
-    }
+  function registerContentRoutes (app) {
+    app.addContentPage(path.join(__dirname, '/pages/local-login.fragment.html'))
+    app.addContentPage(path.join(__dirname, '/pages/logout.fragment.html'))
+  }
 
-    function registerContentRoutes(app) {
-        app.addContentPage(__dirname + '/pages/local-login.fragment.html');
-        app.addContentPage(__dirname + '/pages/logout.fragment.html');
-    }
+  function registerAuthRoutes (app) {
+    var server = app.server
 
-    function registerAuthRoutes(app) {
-        var server = app.server;
+    server.post('/auth/local/login',
+      passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/docs/local-login'
+      })
+    )
 
-        server.post('/auth/local/login',
-            passport.authenticate('local', {
-                successRedirect: '/',
-                failureRedirect: '/docs/local-login'
-            })
-        );
+    server.post('/auth/local/sha1sum', function (req, res) {
+      var value = req.body.value
+      res.jsonp({
+        sha1sum: sha1sum(value)
+      })
+    })
 
-        server.post('/auth/local/sha1sum', function(req, res) {
-            var value = req.body.value;
-            res.jsonp({
-                sha1sum: sha1sum(value)
-            });
-        });
+    app.enableAuthentication({
+      name: 'local',
+      url: '/docs/local-login'
+    })
+  }
 
-        app.enableAuthentication({
-            name: 'local',
-            url: '/docs/local-login'
-        });
-    }
+  function apply (app) {
+    registerStrategy(app)
+    registerContentRoutes(app)
+    registerAuthRoutes(app)
+  }
 
-    function apply(app) {
-        registerStrategy(app);
-        registerContentRoutes(app);
-        registerAuthRoutes(app);
-    }
-
-    return {
-        info,
-        apply,
-        getConfig,
-        setConfig
-    };
+  return {
+    info,
+    apply,
+    getConfig,
+    setConfig
+  }
 }
 
-module.exports = create;
+module.exports = create
--- test/pluginApi-spec.js
+++ test/pluginApi-spec.js
@@ -1,66 +1,66 @@
-var expect = require('chai').expect;
-var plugin = require('../local-auth/plugin.js');
+/* eslint-env mocha */
+const expect = require('chai').expect
+const plugin = require('../local-auth/plugin.js')
 
-describe('Plugin API', function() {
+describe('Plugin API', function () {
+  var instance
 
-    var instance;
+  beforeEach(function () {
+    instance = plugin()
+  })
 
-    beforeEach(function() {
-        instance = plugin();
-    });
+  it('should provide info about the plugin', function () {
+    var actual = instance.info()
+    expect(actual).to.have.property('name')
+    expect(actual).to.have.property('description')
+    expect(actual).to.have.property('keywords')
+  })
 
-    it('should provide info about the plugin', function() {
-        var actual = instance.info();
-        expect(actual).to.have.property('name');
-        expect(actual).to.have.property('description');
-        expect(actual).to.have.property('keywords');
-    });
+  it('should apply methods to the supplied application', function (done) {
+    var expectedTasks = {
+      'addContentPage': false,
+      'serverPost:/auth/local/login': false,
+      'serverPost:/auth/local/sha1sum': false,
+      'enableAuthentication:local:/docs/local-login': false
+    }
 
-    it('should apply methods to the supplied application', function(done) {
-        var expectedTasks = {
-            "addContentPage": false,
-            "serverPost:/auth/local/login": false,
-            "serverPost:/auth/local/sha1sum": false,
-            "enableAuthentication:local:/docs/local-login": false
-        };
-
-        function complete(task) {
-            if (expectedTasks.hasOwnProperty(task)) {
-                expectedTasks[task] = true;
-                var completed = Object.keys(expectedTasks)
-                    .map((task) => expectedTasks[task])
-                    .reduce((previousValue, currentValue, currentIndex, array) => previousValue && currentValue);
-                if (completed) {
-                    done();
-                }
-            } else {
-                console.log('Unexpected task:', task);
-            }
+    function complete (task) {
+      if (expectedTasks.hasOwnProperty(task)) {
+        expectedTasks[task] = true
+        var completed = Object.keys(expectedTasks)
+          .map((task) => expectedTasks[task])
+          .reduce((previousValue, currentValue, currentIndex, array) => previousValue && currentValue)
+        if (completed) {
+          done()
         }
+      } else {
+        console.log('Unexpected task:', task)
+      }
+    }
 
-        var app = {
-            addContentPage: () => complete('addContentPage'),
-            server: {
-                get: (path, req, res) => complete('serverGet:' + path),
-                post: (path, req, res) => complete('serverPost:' + path)
-            },
-            enableAuthentication: (options) => complete('enableAuthentication:' + options.name + ':' + options.url)
-        };
-        instance.apply(app);
-    });
+    var app = {
+      addContentPage: () => complete('addContentPage'),
+      server: {
+        get: (path, req, res) => complete('serverGet:' + path),
+        post: (path, req, res) => complete('serverPost:' + path)
+      },
+      enableAuthentication: (options) => complete('enableAuthentication:' + options.name + ':' + options.url)
+    }
+    instance.apply(app)
+  })
 
-    it('should provide a default config', function() {
-        var actual = instance.getConfig();
-        expect(actual).to.deep.equal({});
-    });
+  it('should provide a default config', function () {
+    var actual = instance.getConfig()
+    expect(actual).to.deep.equal({})
+  })
 
-    it('should allow the default config to be changed', function() {
-        var expected = {
-            validation: true
-        };
-        expect(instance.getConfig()).to.not.deep.equal(expected);
+  it('should allow the default config to be changed', function () {
+    var expected = {
+      validation: true
+    }
+    expect(instance.getConfig()).to.not.deep.equal(expected)
 
-        instance.setConfig(expected);
-        expect(instance.getConfig()).to.deep.equal(expected);
-    });
-});
+    instance.setConfig(expected)
+    expect(instance.getConfig()).to.deep.equal(expected)
+  })
+})
